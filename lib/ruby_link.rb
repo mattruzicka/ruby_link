b58e59ae0d28bae3fe65509e215fe3ab706df5c3
Matt Ruzicka <mattruzicka@gmail.com>
2021-08-04 18:25:20 -0500

    An in-development dependency of the readme_yard gem

---
+++ lib/ruby_link.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_link/version"
+
+module RubyLink
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_link/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RubyLink
+  VERSION = "0.1.0"
+end
